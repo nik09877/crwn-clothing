e24868b047914d051c1bd79ad77b133d9c20be0a
Nikhil Kumar Mishra <nikhilkumarmishra120@gmail.com>
2023-02-07 19:36:08 +0530

    migrated to using styled componets due to css leak and implemented nested routing inside shop-route

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,16 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -23,20 +32,67 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
+//////////////////////////////////////////////////////////////
+//SIGN IN WITH GOOGLE
+//////////////////////////////////////////////////////////////
 const googleProvider = new GoogleAuthProvider();
-
 googleProvider.setCustomParameters({
   prompt: 'select_account',
 });
-
 export const auth = getAuth();
 export const signInWithGooglePopup = () =>
   signInWithPopup(auth, googleProvider);
 export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
-export const db = getFirestore();
+//////////////////////////////////////////////////////////////
+//SIGN IN, SIGN UP AND SIGN OUT WITH EMAIL AND PASSWORD
+//////////////////////////////////////////////////////////////
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
 
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
+
+//////////////////////////////////////////////////////////////
+//GET YOUR DATABASE
+export const db = getFirestore();
+//////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+//ADD COLLECTION AND ITS DOCUMENTS AT ONCE IN BULK
+//////////////////////////////////////////////////////////////
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd,
+  docKey
+) => {
+  const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done');
+};
+
+//////////////////////////////////////////////////////////////
+// CRUD OPERATIONS ON 'USERS' COLLECTION
+//////////////////////////////////////////////////////////////
 export const createUserDocumentFromAuth = async (
   userAuth,
   additionalInformation = {}
@@ -66,19 +122,19 @@ export const createUserDocumentFromAuth = async (
   return userDocRef;
 };
 
-export const createAuthUserWithEmailAndPassword = async (email, password) => {
-  if (!email || !password) return;
+//////////////////////////////////////////////////////////////
+// CRUD OPERATIONS ON 'CATEGORIES' COLLECTION
+//////////////////////////////////////////////////////////////
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
 
-  return await createUserWithEmailAndPassword(auth, email, password);
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+
+  return categoryMap;
 };
-
-export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-  if (!email || !password) return;
-
-  return await signInWithEmailAndPassword(auth, email, password);
-};
-
-export const signOutUser = async () => await signOut(auth);
-
-export const onAuthStateChangedListener = (callback) =>
-  onAuthStateChanged(auth, callback);
