4076d27c1f3076c72a26482e257f0cf22372d6a1
Nikhil Kumar Mishra <nikhilkumarmishra120@gmail.com>
2023-02-16 19:46:34 +0530

    checking something

---
+++ src/firebase-secret.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDbA12ZqUPQSkLvUmnJhgNqCVRb6Kglyv4',
+  authDomain: 'crwn-db-e6798.firebaseapp.com',
+  projectId: 'crwn-db-e6798',
+  storageBucket: 'crwn-db-e6798.appspot.com',
+  messagingSenderId: '96970754250',
+  appId: '1:96970754250:web:ed21869eb1c0a4dfb6cc33',
+};
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -36,14 +36,7 @@ import {
 } from 'firebase/storage';
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyDbA12ZqUPQSkLvUmnJhgNqCVRb6Kglyv4',
-  authDomain: 'crwn-db-e6798.firebaseapp.com',
-  projectId: 'crwn-db-e6798',
-  storageBucket: 'crwn-db-e6798.appspot.com',
-  messagingSenderId: '96970754250',
-  appId: '1:96970754250:web:ed21869eb1c0a4dfb6cc33',
-};
+import { firebaseConfig } from '../../firebase-secret';
 
 const firebaseApp = initializeApp(firebaseConfig);
 const storage = getStorage();
