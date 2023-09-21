45539621772dd13f74906e8a34bc191745e70c04
Nikhil Kumar Mishra <nikhilkumarmishra120@gmail.com>
2023-09-21 12:11:09 +0530

    Stopped tracking .env File

--- src/firebase-secret.js
+++ src/firebase-secret.js
@@ -1,9 +1,9 @@
 //COMMENT PLEASE USE YOUR OWN FIREBASE CONFIG IF YOU ARE CLONING THIS PROJECT
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDbA12ZqUPQSkLvUmnJhgNqCVRb6Kglyv4',
-  authDomain: 'crwn-db-e6798.firebaseapp.com',
-  projectId: 'crwn-db-e6798',
-  storageBucket: 'crwn-db-e6798.appspot.com',
-  messagingSenderId: '96970754250',
-  appId: '1:96970754250:web:ed21869eb1c0a4dfb6cc33',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
